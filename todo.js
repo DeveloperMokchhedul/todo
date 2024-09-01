0f5ebf58b62c18bba5b38f61372b02f5abb43e1d
DeveloperMokchhedul <mokchheduls46@gmail.com>
2024-09-01 10:27:51 +0600

    todo project

---
+++ todo.js
@@ -0,0 +1,46 @@
+const inputData = document.querySelector("#new_To_Do_Item");
+const addData = document.querySelector("#addBtn");
+const parentElement = document.querySelector("#parentElement");
+
+let currentLiElement = null; 
+addData.addEventListener("click", () => {
+    let data = inputData.value.trim();
+    if (data.length == 0) {
+        alert("input field required")
+        return false;
+    }
+
+    if (addData.textContent === "Add") {
+        let todoList = document.createElement("li");
+        todoList.className = "flex justify-between";
+        todoList.innerHTML = 
+        `${data} 
+            <div>
+                <button onclick="handleEdit(this)" class="bg-green-600 text-white px-4 py-1 rounded-md">Edit</button>
+                <button onclick="handleDelete(this)" class="bg-red-600 text-white px-4 py-1 rounded-md">Delete</button>
+            </div>
+        `;
+        parentElement.appendChild(todoList);
+    } else if (currentLiElement !== null) {
+        currentLiElement.textContent = data; 
+        addData.textContent = "Add"; 
+        currentLiElement = null; 
+    } 
+    inputData.value = ""; 
+});
+
+const handleDelete = (deleteData) => {
+    const proParent = deleteData.closest("li");
+    proParent.remove();
+
+};
+
+const handleEdit = (editData) => {
+    let liElement = editData.closest("li").childNodes[0]; 
+    if (liElement) {
+        inputData.value = liElement.textContent.trim(); 
+        addData.textContent = "Save"; 
+        currentLiElement = liElement; 
+    } 
+
+};
